4f6876cc5cd74d9b95a600499b0a0d2910f5a7b2
NguyenLeeee <nguyennlse183418@fpt.edu.vn>
2023-03-21 18:28:12 +0700

    Test

--- src/main.c
+++ src/main.c
@@ -3,7 +3,9 @@
 #include "model/sign_in.h"
 #include "model/directProject.h"
 int main(int argc, char *argv[]) {
-
+    int k;
     char user[] = "lap",pass[]="123456",direct[] = "C:/Users/ADMIN/CLionProjects/music/database/music.db";
     sign_in(user,pass,direct);
+
+
 }
