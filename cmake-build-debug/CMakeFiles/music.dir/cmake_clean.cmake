d2a8ea39db2ffba5447f838ba6aabb3c55b3ac81
Nhat <60876500+UpperMoon0@users.noreply.github.com>
2023-03-22 18:11:47 +0700

    Main menu giai đoạn 1
    - Đổi tên "sample.h" thành "ui_init.h", đây sẽ là file header chung quản lý việc khởi tạo UI khi bắt đầu app.
    - Sắp xếp lại bố cục các file header của FE.
    - Add chức năng chuyển qua lại giữa các "cửa sổ" (box) trong app.
    - Add tạm thời các label của cửa sổ main menu cho mục đích thử nghiệm.

---
+++ cmake-build-debug/CMakeFiles/music.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/music.dir/src/main.c.obj"
+  "CMakeFiles/music.dir/src/main.c.obj.d"
+  "libmusic.dll.a"
+  "music.exe"
+  "music.exe.manifest"
+  "music.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/music.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/model/connectData.h
@@ -0,0 +1,26 @@
+#ifndef MUSIC_CONNECTDATA_H
+#define MUSIC_CONNECTDATA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+#include <string.h>
+
+// check connect with database
+int connectData(char *direct, char *notification, int *status) {
+    sqlite3 *db;
+    char *dbm = direct;
+
+    int rc = sqlite3_open(dbm, &db);
+    if (rc != SQLITE_OK) {
+        strcpy(notification, "Cannot open database");
+        sqlite3_close(db);
+        return 1;
+    }
+
+    strcpy(notification, "Connection is successful!");
+    sqlite3_close(db);
+    return 0;
+}
+
+#endif //MUSIC_CONNECTDATA_H
