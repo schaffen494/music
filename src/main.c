e49cb5fcbe78873601397755a4d01d0a52268341
Lap <hvlap494@gmail.com>
2023-03-25 10:06:30 +0800

    Merge remote-tracking branch 'github/master'

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "query/db_signin.h"
-#include "query/directProject.h"
-#include "view/sample.h"
+
 
 struct threeNum
 {
@@ -13,5 +11,5 @@ int main(int argc, char *argv[]) {
 
     char user[] = "lap",pass[]="123456",direct[] = "C:/Users/ADMIN/CLionProjects/music/database/music.db";
     int *status;
-    db_signin(user,pass,direct,status);
+
 }
