2d8b064544c4635d587a196d68ac0896043f7653
Nhat <60876500+UpperMoon0@users.noreply.github.com>
2023-04-06 14:58:33 +0700

    Selection Menu update

--- src/components/main_menu.c
+++ src/components/main_menu.c
@@ -3,8 +3,22 @@
 //
 
 #include "main_menu.h"
+#include "searchBar.h"
+#include "selection_menu.h"
 
-static void on_login_clicked(GtkButton *button, gpointer user_data) {
+static void delete_all_children_of_container(GtkWidget *content_grid)
+{
+    GList *children_list = gtk_container_get_children(GTK_CONTAINER(content_grid));
+    int i = 0;
+
+    for (; i < g_list_length(children_list); i++)
+    {
+        gtk_container_remove(GTK_CONTAINER(content_grid), g_list_nth_data(children_list, i));
+    }
+}
+
+static void on_login_clicked(GtkButton *button, gpointer user_data)
+{
     // Xử lý sự kiện khi người dùng nhấn nút Đăng nhập
 }
 
@@ -14,19 +28,29 @@ static void on_register_clicked(GtkButton *button, gpointer user_data) {
 
 static void on_search_clicked(GtkButton *button, gpointer user_data) {
     // Xử lý sự kiện khi người dùng nhấn nút Tìm kiếm
+    create_Searchbar(user_data);
 }
 
 void create_menu(GtkWidget *window) {
+    GtkWidget *content_grid;
     GtkWidget *menu_bar;
     GtkWidget *file_menu;
     GtkWidget *file_menu_item;
     GtkWidget *login_menu_item;
     GtkWidget *register_menu_item;
     GtkWidget *search_menu_item;
+    GtkWidget *selection_menu_item;
+
+    // Tạo content grid
+    content_grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(window), content_grid);
+
+    // Tạo selection menu
+    create_selection_menu(content_grid);
 
     // Tạo menu bar
     menu_bar = gtk_menu_bar_new();
-    gtk_container_add(GTK_CONTAINER(window), menu_bar);
+    gtk_grid_attach(GTK_GRID(content_grid), menu_bar, 1, 0, 1, 1);
 
     // Tạo menu File
     file_menu = gtk_menu_new();
@@ -47,5 +71,5 @@ void create_menu(GtkWidget *window) {
     // Tạo menu Search
     search_menu_item = gtk_menu_item_new_with_label("Tìm kiếm");
     gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), search_menu_item);
-    g_signal_connect(search_menu_item, "activate", G_CALLBACK(on_search_clicked), NULL);
+    g_signal_connect(search_menu_item, "activate", G_CALLBACK(on_search_clicked), window);
 }
---
+++ src/components/selection_menu.h
@@ -0,0 +1,71 @@
+//
+// Created by Nhat on 4/5/2023.
+//
+
+#ifndef MUSIC_SELECTION_MENU_H
+#define MUSIC_SELECTION_MENU_H
+
+#include <gtk/gtk.h>
+
+// Create all widgets for the Selection Menu
+void create_selection_menu(GtkWidget *grid)
+{
+    GtkWidget *menu_box,
+              *bottom_box, // Empty box below menu box
+              *avatar,
+              *discover_button,
+              *follow_button,
+              *my_music_button,
+              *upload_button;
+
+    // Create a vertical box to contain the menu grid
+    menu_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 30);
+    gtk_grid_attach(GTK_GRID(grid), menu_box, 0, 1, 1, 1);
+
+    // Create a vertical box to contain the menu grid
+    bottom_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_grid_attach(GTK_GRID(grid), bottom_box, 0, 2, 1, 1);
+    gtk_widget_set_vexpand(bottom_box, TRUE);
+
+    // Create the logo
+    avatar = gtk_image_new_from_file("assets\\logo_minimized.png");
+    gtk_widget_set_name(avatar, "logo");
+    gtk_grid_attach(GTK_GRID(grid), avatar, 0, 0, 1, 1);
+
+    // Create the Discover button
+    discover_button = gtk_button_new_with_label("Discover");
+    gtk_button_set_relief(GTK_BUTTON(discover_button), GTK_RELIEF_NONE);
+    gtk_box_pack_start(GTK_BOX(menu_box), discover_button, FALSE, FALSE, 0);
+
+    // Create the Follow button
+    follow_button = gtk_button_new_with_label("Follow");
+    gtk_button_set_relief(GTK_BUTTON(follow_button), GTK_RELIEF_NONE);
+    gtk_box_pack_start(GTK_BOX(menu_box), follow_button, FALSE, FALSE, 0);
+
+    // Create the My Music button
+    my_music_button = gtk_button_new_with_label("My Music");
+    gtk_button_set_relief(GTK_BUTTON(my_music_button), GTK_RELIEF_NONE);
+    gtk_box_pack_start(GTK_BOX(menu_box), my_music_button, FALSE, FALSE, 0);
+
+    // Create the Upload button
+    upload_button = gtk_button_new_with_label("Upload");
+    gtk_button_set_relief(GTK_BUTTON(upload_button), GTK_RELIEF_NONE);
+    gtk_box_pack_start(GTK_BOX(menu_box), upload_button, FALSE, FALSE, 0);
+
+    // Load css file
+    GtkCssProvider *provider = gtk_css_provider_new();
+    GdkDisplay *display = gdk_display_get_default ();
+    GdkScreen *screen = gdk_display_get_default_screen (display);
+    GError *error = NULL;
+
+    gtk_css_provider_load_from_path(provider, "src\\components\\selection_menu.css", &error);
+
+    if (error)
+    {
+        g_warning ("%s", error->message);
+        g_clear_error (&error);
+    }
+
+    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+}
+#endif //MUSIC_SELECTION_MENU_H
