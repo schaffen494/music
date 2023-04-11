c090c1cac04758f612a0a1d318a726634b5e5d15
Nhat <60876500+UpperMoon0@users.noreply.github.com>
2023-04-11 20:57:33 +0700

    Add files via upload

--- src/view/mainboard_view.c
+++ src/view/mainboard_view.c
@@ -5,6 +5,7 @@
 #include "mainboard_view.h"
 #include "src/components/selection_menu.h"
 #include "src/components/musicBar.h"
+#include "src/components/my_music.h"
 
 GtkWidget *main_board_window;
 void main_board_show()
@@ -23,11 +24,21 @@ void main_board_show()
     GtkWidget* fixed = gtk_fixed_new();
     gtk_container_add(GTK_CONTAINER(main_board_window), fixed);
 
+    // Tạo selection menu
     create_selection_menu(fixed);
+
+    // Tạo music bar
     create_MusicBar(fixed);
+
+    // Tạo tab my music
+    GtkWidget *my_music_sub_fixed = gtk_fixed_new();
+    gtk_fixed_put(GTK_FIXED(fixed), my_music_sub_fixed, 0, 0);
+    create_my_music_tab(my_music_sub_fixed);
+
     // Xử lý sự kiện đóng cửa sổ
     g_signal_connect(G_OBJECT(main_board_window), "destroy", G_CALLBACK(exit), NULL);
 
     gtk_widget_show_all(main_board_window);
+    hide_my_music_tab();
     gtk_main();
 }
